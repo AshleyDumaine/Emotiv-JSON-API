7f6d4a1d77c7308b530a66b4790038a229cc08e1
Ashley <ashley.dumaine@uconn.edu>
2015-04-03 04:34:14 -0400

    Made API_Main a runnable

--- Emotiv-JSON-API/src/API_Main.java
+++ Emotiv-JSON-API/src/API_Main.java
@@ -14,161 +14,174 @@ import org.json.*;
 import com.sun.jna.Pointer;
 import com.sun.jna.ptr.*;
 
-public class API_Main {      
+public class API_Main implements Runnable {      
 	public static void main(String[] args) throws Exception {
-		ServerSocket serverSocket = new ServerSocket(4444);
-		Pointer eEvent			= Edk.INSTANCE.EE_EmoEngineEventCreate();
-		Pointer eState			= Edk.INSTANCE.EE_EmoStateCreate();
-		IntByReference userID 	= new IntByReference(0);
-		IntByReference contactQuality = new IntByReference(0);
-		IntByReference chargeLevel = new IntByReference(0);
-		int state = 0;
-		String strParams = "", line = "";
-		HashMap<Integer, String> cognitivMap = new HashMap<Integer, String>();
-		HashMap<String, Function<Pointer, Float>> affectivMap = new HashMap<String, Function<Pointer, Float>>();
-		HashMap<String, Function<Pointer, Number>> expressivMap = new HashMap<String, Function<Pointer, Number>>();
-		BufferedReader br  = null;
-		HashSet<String> params = new HashSet<String>(); //use HashSet for O(1) lookup time
-		String[] wirelessSignalStatus = {"NO_SIGNAL", "BAD_SIGNAL", "GOOD_SIGNAL"};
-		
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			public void run() {
+
+	}
+
+	@Override
+	public void run() {
+		ServerSocket serverSocket;
+		try {
+			serverSocket = new ServerSocket(3333);
+			Pointer eEvent			= Edk.INSTANCE.EE_EmoEngineEventCreate();
+			Pointer eState			= Edk.INSTANCE.EE_EmoStateCreate();
+			IntByReference userID 	= new IntByReference(0);
+			IntByReference contactQuality = new IntByReference(0);
+			IntByReference chargeLevel = new IntByReference(0);
+			int state = 0;
+			String strParams = "", line = "";
+			HashMap<Integer, String> cognitivMap = new HashMap<Integer, String>();
+			HashMap<String, Function<Pointer, Float>> affectivMap = new HashMap<String, Function<Pointer, Float>>();
+			HashMap<String, Function<Pointer, Number>> expressivMap = new HashMap<String, Function<Pointer, Number>>();
+			BufferedReader br  = null;
+			HashSet<String> params = new HashSet<String>(); //use HashSet for O(1) lookup time
+			String[] wirelessSignalStatus = {"NO_SIGNAL", "BAD_SIGNAL", "GOOD_SIGNAL"};
+
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+				public void run() {
+					try {
+						serverSocket.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+
+			//cognitiv
+			br = new BufferedReader(new FileReader((new File("CognitivEvents.txt")).getAbsolutePath()));
+			while ((line = br.readLine()) != null) {
+				String parts[] = line.split("\t");
+				cognitivMap.put(Integer.decode(parts[0]), parts[1]);
+			}
+			br.close();
+
+			//affectiv
+			affectivMap.put("Frustration", EmoState.INSTANCE::ES_AffectivGetFrustrationScore);
+			affectivMap.put("Meditation", EmoState.INSTANCE::ES_AffectivGetMeditationScore);
+			affectivMap.put("EngagementBoredom", EmoState.INSTANCE::ES_AffectivGetEngagementBoredomScore);
+			affectivMap.put("ExcitementShortTerm", EmoState.INSTANCE::ES_AffectivGetExcitementShortTermScore);
+			affectivMap.put("ExcitementLongTerm", EmoState.INSTANCE::ES_AffectivGetExcitementLongTermScore);
+
+			//expressiv
+			expressivMap.put("Blink", EmoState.INSTANCE::ES_ExpressivIsBlink);
+			expressivMap.put("LeftWink", EmoState.INSTANCE::ES_ExpressivIsLeftWink);
+			expressivMap.put("RightWink", EmoState.INSTANCE::ES_ExpressivIsRightWink);
+			expressivMap.put("LookingLeft", EmoState.INSTANCE::ES_ExpressivIsLookingLeft);
+			expressivMap.put("LookingRight", EmoState.INSTANCE::ES_ExpressivIsLookingRight);
+			expressivMap.put("LookingUp", EmoState.INSTANCE::ES_ExpressivIsLookingUp);
+			expressivMap.put("LookingDown", EmoState.INSTANCE::ES_ExpressivIsLookingDown);
+			expressivMap.put("EyesOpen", EmoState.INSTANCE::ES_ExpressivIsEyesOpen);
+			expressivMap.put("Clench", EmoState.INSTANCE::ES_ExpressivGetClenchExtent);
+			expressivMap.put("Smile", EmoState.INSTANCE::ES_ExpressivGetSmileExtent);
+			expressivMap.put("EyebrowRaise", EmoState.INSTANCE::ES_ExpressivGetEyebrowExtent);
+
+			if (Edk.INSTANCE.EE_EngineConnect("Emotiv Systems-5") != EdkErrorCode.EDK_OK.ToInt()) {
+				System.out.println("Emotiv Engine start up failed.");
+				return;
+			}
+
+			while (true) {
+				//will block, waiting for a client to send data to before it reads EEG data
 				try {
-					serverSocket.close();
-				} catch (IOException e) {
+					Socket connectionSocket = serverSocket.accept();
+					BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+					DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+					// client tells server what events it wants to be updated about
+					strParams = inFromClient.readLine();
+
+					//get each param from comma separated input and add them to the HashSet
+					StringTokenizer st = new StringTokenizer(strParams, ", ");
+					while(st.hasMoreTokens())
+						params.add(st.nextToken());
+					System.out.println("Client wants updates on: " + params.toString()); 
+
+					while (true) { 
+						//concatenated response of headset data and EEG events
+						JSONObject response = new JSONObject(); 
+						JSONObject headsetData = new JSONObject();
+						//EEG Events
+						JSONObject emoStateData = new JSONObject();
+						JSONArray expressivArray = new JSONArray();
+						JSONArray affectivArray = new JSONArray();
+
+						state = Edk.INSTANCE.EE_EngineGetNextEvent(eEvent);
+						// New event needs to be handled
+						if (state == EdkErrorCode.EDK_OK.ToInt()) {
+							int eventType = Edk.INSTANCE.EE_EmoEngineEventGetType(eEvent);
+							Edk.INSTANCE.EE_EmoEngineEventGetUserId(eEvent, userID);
+							// Log the EmoState if it has been updated
+							if (eventType == Edk.EE_Event_t.EE_EmoStateUpdated.ToInt()) {
+								Edk.INSTANCE.EE_EmoEngineEventGetEmoState(eEvent, eState);
+								headsetData.put("Timestamp", EmoState.INSTANCE.ES_GetTimeFromStart(eState));
+								headsetData.put("EmoState", userID.getValue());
+								headsetData.put("WirelessSignalStatus", wirelessSignalStatus[EmoState.INSTANCE.ES_GetWirelessSignalStatus(eState)]);
+								//headsetData.put("SensorContactQuality", EmoState.INSTANCE.ES_GetContactQualityFromAllChannels(eState, contactQuality, numChannels));
+								headsetData.put("IsHeadsetOnCorrectly", EmoState.INSTANCE.ES_GetHeadsetOn(eState));
+								IntByReference maxChargeLevel = new IntByReference(); //maxChargeLevel = ???
+								EmoState.INSTANCE.ES_GetBatteryChargeLevel(eState, chargeLevel, maxChargeLevel);
+								headsetData.put("BatteryChargeLevel", chargeLevel.getValue());
+								response.put("HeadsetData", headsetData);
+								for (Entry<String, Function<Pointer, Number>> entry : expressivMap.entrySet()) {
+									if (params.contains(entry.getKey())) {
+										float returnVal = (entry.getValue().apply(eState)).floatValue();
+										if (returnVal > 0) {
+											JSONObject jo = new JSONObject();
+											jo.put(entry.getKey(), returnVal);
+											expressivArray.put(jo);
+										}
+									}	
+								}
+								if (params.contains("EyeLocation")) {
+									JSONObject jo = new JSONObject();
+									FloatByReference eyeXLocation = new FloatByReference(0);
+									FloatByReference eyeYLocation = new FloatByReference(0);
+									EmoState.INSTANCE.ES_ExpressivGetEyeLocation(eState, eyeXLocation, eyeYLocation);
+									Point2D.Float eyeLocation = new Point2D.Float(eyeXLocation.getValue(),eyeYLocation.getValue());
+									jo.put("EyeLocation", eyeLocation); 
+									expressivArray.put(jo);
+								}
+								emoStateData.put("Expressiv", expressivArray);
+
+								for (Entry<String, Function<Pointer, Float>> entry : affectivMap.entrySet()) {
+									if (params.contains(entry.getKey())) {
+										JSONObject jo = new JSONObject();
+										jo.put(entry.getKey(), (entry.getValue().apply(eState)).floatValue());
+										affectivArray.put(jo);
+									}	
+								}
+								emoStateData.put("Affectiv", affectivArray);
+
+								JSONObject cognitivData = new JSONObject();
+								if (params.contains("Cognitiv")) {
+									int currentAction = EmoState.INSTANCE.ES_CognitivGetCurrentAction(eState);
+									if (cognitivMap.containsKey(currentAction))
+										cognitivData.put(cognitivMap.get(currentAction), EmoState.INSTANCE.ES_CognitivGetCurrentActionPower(eState));
+								}
+								emoStateData.put("Cognitiv", cognitivData);
+								response.put("EmoStateData", emoStateData);
+								outToClient.writeBytes(response.toString() + "\n"); //write to socket only at end
+							}
+						}
+						else if (state != EdkErrorCode.EDK_NO_EVENT.ToInt()) {
+							outToClient.writeBytes("Internal error in Emotiv Engine!");
+							break;
+						}
+					}
+					Edk.INSTANCE.EE_EngineDisconnect();
+					System.out.println("Disconnected!");
+				}
+				catch (SocketException se) {
+					System.out.println("Socket error");
+					se.printStackTrace();
+				} catch (JSONException e) {
 					e.printStackTrace();
 				}
 			}
-		});
-		
-		//cognitiv
-		br = new BufferedReader(new FileReader((new File("CognitivEvents.txt")).getAbsolutePath()));
-		while ((line = br.readLine()) != null) {
-			String parts[] = line.split("\t");
-			cognitivMap.put(Integer.decode(parts[0]), parts[1]);
-		}
-		br.close();
-		
-		//affectiv
-		affectivMap.put("Frustration", EmoState.INSTANCE::ES_AffectivGetFrustrationScore);
-		affectivMap.put("Meditation", EmoState.INSTANCE::ES_AffectivGetMeditationScore);
-		affectivMap.put("EngagementBoredom", EmoState.INSTANCE::ES_AffectivGetEngagementBoredomScore);
-		affectivMap.put("ExcitementShortTerm", EmoState.INSTANCE::ES_AffectivGetExcitementShortTermScore);
-		affectivMap.put("ExcitementLongTerm", EmoState.INSTANCE::ES_AffectivGetExcitementLongTermScore);
-		
-		//expressiv
-		expressivMap.put("Blink", EmoState.INSTANCE::ES_ExpressivIsBlink);
-		expressivMap.put("LeftWink", EmoState.INSTANCE::ES_ExpressivIsLeftWink);
-		expressivMap.put("RightWink", EmoState.INSTANCE::ES_ExpressivIsRightWink);
-		expressivMap.put("LookingLeft", EmoState.INSTANCE::ES_ExpressivIsLookingLeft);
-		expressivMap.put("LookingRight", EmoState.INSTANCE::ES_ExpressivIsLookingRight);
-		expressivMap.put("LookingUp", EmoState.INSTANCE::ES_ExpressivIsLookingUp);
-		expressivMap.put("LookingDown", EmoState.INSTANCE::ES_ExpressivIsLookingDown);
-		expressivMap.put("EyesOpen", EmoState.INSTANCE::ES_ExpressivIsEyesOpen);
-		expressivMap.put("Clench", EmoState.INSTANCE::ES_ExpressivGetClenchExtent);
-		expressivMap.put("Smile", EmoState.INSTANCE::ES_ExpressivGetSmileExtent);
-		expressivMap.put("EyebrowRaise", EmoState.INSTANCE::ES_ExpressivGetEyebrowExtent);
-		
-		if (Edk.INSTANCE.EE_EngineConnect("Emotiv Systems-5") != EdkErrorCode.EDK_OK.ToInt()) {
-			System.out.println("Emotiv Engine start up failed.");
-			return;
-		}
-
-		while (true) {
-			//will block, waiting for a client to send data to before it reads EEG data
-			try {
-				Socket connectionSocket = serverSocket.accept();
-				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-				DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-
-				// client tells server what events it wants to be updated about
-				strParams = inFromClient.readLine();
-
-				//get each param from comma separated input and add them to the HashSet
-				StringTokenizer st = new StringTokenizer(strParams, ", ");
-				while(st.hasMoreTokens())
-					params.add(st.nextToken());
-				System.out.println("Client wants updates on: " + params.toString()); 
-
-				while (true) { 
-					//concatenated response of headset data and EEG events
-					JSONObject response = new JSONObject(); 
-					JSONObject headsetData = new JSONObject();
-					//EEG Events
-					JSONObject emoStateData = new JSONObject();
-					JSONArray expressivArray = new JSONArray();
-					JSONArray affectivArray = new JSONArray();
-
-					state = Edk.INSTANCE.EE_EngineGetNextEvent(eEvent);
-					// New event needs to be handled
-					if (state == EdkErrorCode.EDK_OK.ToInt()) {
-						int eventType = Edk.INSTANCE.EE_EmoEngineEventGetType(eEvent);
-						Edk.INSTANCE.EE_EmoEngineEventGetUserId(eEvent, userID);
-						// Log the EmoState if it has been updated
-						if (eventType == Edk.EE_Event_t.EE_EmoStateUpdated.ToInt()) {
-							Edk.INSTANCE.EE_EmoEngineEventGetEmoState(eEvent, eState);
-							headsetData.put("Timestamp", EmoState.INSTANCE.ES_GetTimeFromStart(eState));
-							headsetData.put("EmoState", userID.getValue());
-							headsetData.put("WirelessSignalStatus", wirelessSignalStatus[EmoState.INSTANCE.ES_GetWirelessSignalStatus(eState)]);
-							//headsetData.put("SensorContactQuality", EmoState.INSTANCE.ES_GetContactQualityFromAllChannels(eState, contactQuality, numChannels));
-							headsetData.put("IsHeadsetOnCorrectly", EmoState.INSTANCE.ES_GetHeadsetOn(eState));
-							IntByReference maxChargeLevel = new IntByReference(); //maxChargeLevel = ???
-							EmoState.INSTANCE.ES_GetBatteryChargeLevel(eState, chargeLevel, maxChargeLevel);
-							headsetData.put("BatteryChargeLevel", chargeLevel.getValue());
-							response.put("HeadsetData", headsetData);
-							for (Entry<String, Function<Pointer, Number>> entry : expressivMap.entrySet()) {
-								if (params.contains(entry.getKey())) {
-									float returnVal = (entry.getValue().apply(eState)).floatValue();
-									if (returnVal > 0) {
-										JSONObject jo = new JSONObject();
-										jo.put(entry.getKey(), returnVal);
-										expressivArray.put(jo);
-									}
-								}	
-							}
-							if (params.contains("EyeLocation")) {
-								JSONObject jo = new JSONObject();
-								FloatByReference eyeXLocation = new FloatByReference(0);
-								FloatByReference eyeYLocation = new FloatByReference(0);
-								EmoState.INSTANCE.ES_ExpressivGetEyeLocation(eState, eyeXLocation, eyeYLocation);
-								Point2D.Float eyeLocation = new Point2D.Float(eyeXLocation.getValue(),eyeYLocation.getValue());
-								jo.put("EyeLocation", eyeLocation); 
-								expressivArray.put(jo);
-							}
-							emoStateData.put("Expressiv", expressivArray);
-
-							for (Entry<String, Function<Pointer, Float>> entry : affectivMap.entrySet()) {
-								if (params.contains(entry.getKey())) {
-									JSONObject jo = new JSONObject();
-									jo.put(entry.getKey(), (entry.getValue().apply(eState)).floatValue());
-									affectivArray.put(jo);
-								}	
-							}
-							emoStateData.put("Affectiv", affectivArray);
-
-							JSONObject cognitivData = new JSONObject();
-							if (params.contains("Cognitiv")) {
-								int currentAction = EmoState.INSTANCE.ES_CognitivGetCurrentAction(eState);
-								if (cognitivMap.containsKey(currentAction))
-									cognitivData.put(cognitivMap.get(currentAction), EmoState.INSTANCE.ES_CognitivGetCurrentActionPower(eState));
-							}
-							emoStateData.put("Cognitiv", cognitivData);
-							response.put("EmoStateData", emoStateData);
-							outToClient.writeBytes(response.toString() + "\n"); //write to socket only at end
-						}
-					}
-					else if (state != EdkErrorCode.EDK_NO_EVENT.ToInt()) {
-						outToClient.writeBytes("Internal error in Emotiv Engine!");
-						break;
-					}
-				}
-				Edk.INSTANCE.EE_EngineDisconnect();
-				System.out.println("Disconnected!");
-			}
-			catch (SocketException se) {
-				System.out.println("Socket error");
-				se.printStackTrace();
-			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
 	}
+
 }
--- Emotiv-JSON-API/src/TestClient.java
+++ Emotiv-JSON-API/src/TestClient.java
@@ -1,19 +1,22 @@
 import java.net.Socket;
 import java.io.*;
 
+import org.json.JSONObject;
+
 public class TestClient {
 	public static void main(String argv[]) throws Exception
 	{
 		String params;
 		String JSONResponse;
 		BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-		Socket clientSocket = new Socket("localhost", 4444);
+		Socket clientSocket = new Socket("localhost", 3333);
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		params = inFromUser.readLine();
 		outToServer.writeBytes(params + '\n');
 		while ((JSONResponse = inFromServer.readLine()) != null) {
-			System.out.println(JSONResponse);
+			JSONObject obj = new JSONObject(JSONResponse);
+			System.out.println(obj);
 		}
 		clientSocket.close();
 		inFromUser.close();
